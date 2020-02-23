d9e3559789b02c15612ab1aabde4fbf1c99023bd
Yves Matta <Yves.Matta7@gmail.com>
2020-02-23 16:58:08 -0500

    remove deploy from travis scripts and add defensive defaults from fetch requests

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -15,12 +15,12 @@ class Fib extends Component {
 
   async fetchValues() {
     const values = await axios.get('/api/values/current');
-    this.setState({ values: values.data });
+    this.setState({ values: values.data || {} });
   }
 
   async fetchIndexes() {
     const seenIndexes = await axios.get('/api/values/all');
-    this.setState({ seenIndexes: seenIndexes.data });
+    this.setState({ seenIndexes: seenIndexes.data || [] });
   }
 
   handleChangeIndex = event => {
