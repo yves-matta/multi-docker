18414431bea925fabf59df22b57dcbcc273c4454
Yves Matta <Yves.Matta7@gmail.com>
2020-03-20 17:49:19 -0400

    change dockerhub username

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
-docker build -t yvesjudematta/multi-client:latest -t yvesjudematta/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t yvesjudematta/multi-server:latest -t yvesjudematta/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t yvesjudematta/multi-worker:latest -t yvesjudematta/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push yvesjudematta/multi-client:latest
-docker push yvesjudematta/multi-client:$SHA
-docker push yvesjudematta/multi-server:latest
-docker push yvesjudematta/multi-server:$SHA
-docker push yvesjudematta/multi-worker:latest
-docker push yvesjudematta/multi-worker:$SHA
+docker build -t yvesmatta/multi-client:latest -t yvesmatta/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t yvesmatta/multi-server:latest -t yvesmatta/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t yvesmatta/multi-worker:latest -t yvesmatta/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push yvesmatta/multi-client:latest
+docker push yvesmatta/multi-client:$SHA
+docker push yvesmatta/multi-server:latest
+docker push yvesmatta/multi-server:$SHA
+docker push yvesmatta/multi-worker:latest
+docker push yvesmatta/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/client-deployment client=yvesmatta/multi-client:$SHA
 kubectl set image deployments/server-deployment server=yvesmatta/multi-server:$SHA
